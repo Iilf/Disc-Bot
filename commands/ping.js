2cbca6a40d02600100dfa8cebc8c080f6b2f8bd7
Iilf <14dziegielewskiszy@ab-ed.org>
2025-12-12 02:32:29 +0000

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,13 @@
+// commands/ping.js
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
+  async execute(interaction) {
+    const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+    const latency = sent.createdTimestamp - interaction.createdTimestamp;
+    await interaction.editReply(`Pong! ws: ${Math.round(interaction.client.ws.ping)}ms | rtt: ${latency}ms`);
+  }
+};
